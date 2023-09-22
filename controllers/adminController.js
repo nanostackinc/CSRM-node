88f5010170862571fba794a31cf623755d848a39
Rafidimas17 <rafidimas1706@gmail.com>
2023-09-22 19:46:48 +0700

    add modal for add student and edit student

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,13 @@
+const dataStudent = require("../json/data.json");
+
 const viewStudent = async (req, res) => {
   // Membuat objek data yang akan dikirimkan ke tampilan
-
+  //   console.log(dataStudent);
+  const student = dataStudent.students;
   // Mengirimkan data ke tampilan
-  res.render("admin/list-student/view_listStudent");
+  res.render("admin/list-student/view_student", {
+    student,
+  });
 };
 
 module.exports = {
